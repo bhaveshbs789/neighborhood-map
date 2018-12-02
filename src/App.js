bf221426fd299bd8ea47c8eef1905b7b333fff50
Bhavesh Sharma <bhaveshbs789@gmail.com>
2018-12-02 11:17:28 +0530

    Props for GoogleMap component

--- src/App.js
+++ src/App.js
@@ -5,24 +5,53 @@ import * as SquareAPI from './SquareAPI'
 
 class App extends Component {
 
-  componentDidMount(){
-  	SquareAPI.searchVenue({
-  		near:'Chicago,IL',
-  		query:'tacos',
-  		limit: 10
-  	}).then(results => console.log(results))
-
-  	SquareAPI.getVenueDetails("50dbf6b3e4b08766dcc9f835")
-  	.then(result => console.log(result))
-
-  	SquareAPI.getVenuePhoto("50dbf6b3e4b08766dcc9f835")
-  	.then(result => console.log(result))
+  state = {
+  	venues : [],
+  	markers : [],
+  	center : [],
+  	zoom : 14
   }
 
+  /* Destructuring in the promise 
+   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+   */
+
+  componentDidMount(){
+  	SquareAPI.searchVenue({
+  		near:'Udaipur,IN',
+  		query:'palace',
+  		limit: 10  		
+  	}).then(results => {
+  		const { venues } = results.response;
+  		const { center } = results.response.geocode.feature.geometry;
+  		const { bounds } = results.response.geocode.feature.geometry;
+  		// const venues = results.response.venues;
+  		// const center = results.response.geocode.feature.geometry;  		
+  		// console.log(Array.isArray(venues));
+  		console.log(results);
+  		const markers = venues.map(venue => {
+  			return {
+  				lat: venue.location.lat,
+  				lng: venue.location.lng
+  			}
+  		});
+  		// console.log(markers);
+  		// console.log(center);
+  		this.setState({venues: venues, markers: markers, center: center})
+  	})
+
+  	// SquareAPI.getVenueDetails("50dbf6b3e4b08766dcc9f835")
+  	// .then(result => console.log(result))
+
+  	// SquareAPI.getVenuePhoto("50dbf6b3e4b08766dcc9f835")
+  	// .then(result => console.log(result))
+  }
+
+  //https://stackoverflow.com/questions/31048953/what-do-these-three-dots-in-react-do
   render() {
     return (
       <div className="App">
-        <Map />
+        <Map {...this.state}/>
       </div>
     );
   }
--- src/Map.js
+++ src/Map.js
@@ -2,18 +2,30 @@ import React, { Component } from 'react'
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
 const MyMapComponent = withScriptjs(withGoogleMap((props) => (
-	<GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-    	{props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+
+	<GoogleMap defaultZoom={8} 
+	    	defaultCenter={{ lat: -34.397, lng: 150.644 }}
+	    	zoom={props.zoom}
+	    	center={props.center}
+	    	>
+    		{props.markers.map((marker,index) => 
+    			<Marker 
+    			key={index} 
+    			position={{ lat: marker.lat, lng: marker.lng }}
+    			/>)
+    		}
   	</GoogleMap>
 	))
 );
   
 class Map extends Component {
+
 	render() {
 		return (
 			<MyMapComponent
+			  {...this.props}
 			  isMarkerShown
-			  googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAcMflL553xnoMM7BcgV4X9rUtDw0yeyFQ"
+			  googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDvxYjj-1V6ARbUow3UKS-uRpRk_SpN9mQ"
 			  loadingElement={<div style={{ height: `100%` }} />}
 			  containerElement={<div style={{ height: `100%`, width:`100%` }} />}
 			  mapElement={<div style={{ height: `100%` }} />}
